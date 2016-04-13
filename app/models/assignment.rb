78567583afbb244e7e99ef0cb4a7e069d4bd4efb
juan <juskhc@gmail.com>
2016-04-13 17:49:18 -0500

    vistas

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,8 @@
 class Assignment < ActiveRecord::Base
 	has_many :students_assignments
-	has_many :teachers_assignments
+	has_many :techers_assignments
 	has_many :students, through: :students_assignments
-	has_many :teachers, through: :teachers_assignments
+	has_many :teachers, through: :techers_assignments
 	has_many :homeworks
 
 	validates :name, presence: true
